ae0983973a5c938485bc78373a314722e6440008
wambetsa emmanuel <wambetsae@gmail.com>
2023-11-20 01:27:39 +0300

    string strlen()

--- strings.c
+++ strings.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /*program entry*/
 int main()
@@ -6,5 +7,9 @@ int main()
     /*strings always have null terminator, \0 */
     char hi[6] = {'H','e','l','l','o','\0'};
     printf("Hi Greeting is : %s\n", hi);
+
+    int i = strlen(hi);
+    printf("Strlen of our char is : %d\n", i);
+
     return 0;
 }
