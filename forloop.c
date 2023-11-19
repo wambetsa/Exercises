16168533d7f08a1bbb6b203f23006de055fffbe6
wambetsa emmanuel <wambetsae@gmail.com>
2023-11-19 17:21:19 +0300

    for loop statement

---
+++ forloop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*entry point of our program*/
+int main()
+{
+    int i;
+
+    for (i = 0; i <= 20; i++)
+    {
+        printf("I is : %d\n", i);
+    }
+    
+    return 0;
+}
