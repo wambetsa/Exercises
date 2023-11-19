3286d321530892c08e2a9c9af44fc21e6b4d0f09
wambetsa emmanuel <wambetsae@gmail.com>
2023-11-19 15:02:39 +0300

    constants in c

---
+++ const.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*constants in c*/
+int main()
+{
+    const int length = 33;
+    const int width = 20;
+    float area = (length * width);
+
+    printf("Area of Rectange = %f\n", area);
+    return 0;
+}
