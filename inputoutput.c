27a811cc1c88d139ebe814ba3ec879ce1ea2cae7
wambetsa emmanuel <wambetsae@gmail.com>
2023-11-20 17:04:36 +0300

    input output getchar() and putchar()

---
+++ inputoutput.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*entry point*/
+int main ()
+{
+    int a, b, c;
+    printf("Enter value: ");
+    a = getchar();
+    printf("Your entry was ");
+    putchar(a);
+    
+    return 0;
+}
